b9b3df68739f9a88152cc3ea689155740f44e973
Aleksandra Tsaregorodtseva <alizandar@rambler.ru>
2022-10-05 21:35:56 +0300

    fixed id problems

--- main.py
+++ main.py
@@ -2,20 +2,34 @@ with open('вк токен.txt', 'r') as file_object:
     vk_token = file_object.read().strip()
 with open('яндекс диск токен.txt', 'r') as file_object:
     ya_token = file_object.read().strip()
+with open('id.txt', 'r') as file_object:
+    user_id = file_object.read().strip()
 import requests
 from pprint import pprint
 import time
 from tqdm import tqdm
 import json
 
-def show_profile_photo(user_id, vk_token):
+def user_id_int():
+    url = "https://api.vk.com/method/users.get"
+    params = {
+        'user_ids': user_id,
+        "access_token": vk_token,
+        "v": "5.131"
+    }
+    response = requests.get(url, params=params)
+    res = response.json()
+    return res['response'][0]['id']
+user = user_id_int()
+def show_profile_photo(user, vk_token):
     url = "https://api.vk.com/method/photos.get"
     params = {
-        "user_ids": user_id,
+        "owner_id": user,
         "access_token": vk_token,
         "v": "5.131",
         "extended":"1",
-        "album_id":"profile"
+        "album_id":"profile",
+        "count": 5
     }
     response = requests.get(url, params=params)
     res = response.json()
@@ -62,7 +76,7 @@ def upload_files_to_YD(ya_token):
                 "path": "/vk"
             }
     response_for_folder = requests.put("https://cloud-api.yandex.net/v1/disk/resources", headers=headers, params=params_for_folder, timeout=10)
-    f_show_profile_photo = show_profile_photo('atsaregorodtseva', vk_token)
+    f_show_profile_photo = show_profile_photo(user, vk_token)
     for photos in f_show_profile_photo:
         number_of_photos = int(input("Количество фото для загрузки: "))
         for el in tqdm(photos[0:number_of_photos], desc= 'photo loading in progress'):
@@ -70,7 +84,7 @@ def upload_files_to_YD(ya_token):
             file_name = el['file_name']
             params = {
                     'url':path,
-                    'path':f"/vk/{file_name}'",
+                    'path':f"/vk/{file_name}",
                     'name': file_name,
                     'overwrite':'false'
                 }
@@ -78,5 +92,4 @@ def upload_files_to_YD(ya_token):
             time.sleep(0.5)
         print('Uploading is finished')
         break
-show_profile_photo('atsaregorodtseva', vk_token)
 upload_files_to_YD(ya_token)
